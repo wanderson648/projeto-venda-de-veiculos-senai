22df053282ae5fffde2746687108acb97b301ee7
Wanderson Oliveira <testepedroca@gmail.com>
2021-11-03 19:25:02 -0300

    projeto-senai

---
+++ script.js
@@ -0,0 +1,26 @@
+
+document.querySelector('.check').addEventListener('click', ()=> {
+  const modelo = document.querySelector('.modelo').value;
+  const preco = Number(document.querySelector('.preco').value);
+
+  if(!modelo) {
+    alert('É necessário passar o modelo do carro!');
+  } 
+  else if (!preco) {
+    alert('É necessário passar o valor do carro!');
+  }
+  else {
+    let valorEntrada = preco * 0.5;
+    let valorParcelas = (preco - valorEntrada) / 60; 
+    let juros = valorParcelas * 0.7;
+
+    let valorTotal = valorParcelas + juros;
+
+    document.querySelector('.message').textContent =  modelo;
+    
+    document.querySelector('.entrada').textContent = valorEntrada.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL'});
+
+    document.querySelector('.parcela').textContent = valorTotal.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL'}); 
+  }
+
+});
